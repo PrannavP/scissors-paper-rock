ecae9346f77c70201eaa1789aa30cbdff46ca104
PrannavP <ramsarilekhi@gmail.com>
2022-12-06 16:34:16 +0545

    removed sounds

--- game.py
+++ game.py
@@ -1,13 +1,17 @@
 import random
 from playsound import playsound
 
-# userScore = 0
-# computerScore = 0
+
+
+# ask rounds
+gameRounds = int(input("How many rounds do you want to play?: "))
 
 # take user's choice
-userchoice = input("Enter your choice: ")
-lowercased_userchoice = userchoice.lower()
-# print(lowercased_userchoice)
+def take_user_input():
+    global userchoice
+    userchoice = input("Enter your choice: ")
+    lowercased_userchoice = userchoice.lower()
+    # print(lowercased_userchoice)
 
 def check_user_input():
     if userchoice == 'rock':
@@ -19,7 +23,7 @@ def check_user_input():
     else:
         print("Invalid choice!!")
 
-check_user_input()
+# check_user_input()
 
 # computer logic
 computer_choice = int(random.random() * 3) # Prints number between 0 and 2
@@ -27,25 +31,32 @@ computer_choice = int(random.random() * 3) # Prints number between 0 and 2
 def check_computer_choice():
     if computer_choice == 0:
         print("Computer chose rock!")
+
     elif computer_choice == 1:
         print("Computer chose paper!")
+
     elif computer_choice == 2:
         print("Computer chose scissor!")
+
     else:
         print("Computer crashed!!")
 
-check_computer_choice()
+# check_computer_choice()
 
 # Compare choices of computer and player
 
 def compare_choices():
+    # Initial scores
     userScore = 0
     computerScore = 0
+
     # Draw case
     if computer_choice == 0 and userchoice == "rock":
         print("DRAW!")
+
     elif computer_choice == 1 and userchoice == "paper":
         print("DRAW!")
+
     elif computer_choice == 2 and userchoice == "scissor":
         print("DRAW!")
     
@@ -54,35 +65,54 @@ def compare_choices():
         print("User won!")
         userScore += 1
         print(f"User Score: { userScore }")
-        playsound('sounds/victory.mp3')
+        # playsound('sounds/victory.mp3')
+
     elif computer_choice == 1 and userchoice == "scissor":
         print("User won!")
         userScore += 1
         print(f"User Score: { userScore }")
-        playsound('sounds/victory.mp3')
+        # playsound('sounds/victory.mp3')
+
     elif computer_choice == 2 and userchoice == "rock":
         print("User won!")
         userScore += 1
         print(f"User Score: { userScore }")
-        playsound('sounds/victory.mp3')
+        # playsound('sounds/victory.mp3')
     
     # Computer win case
     elif computer_choice == 0 and userchoice == "scissor":
         print("Computer won!")
         computerScore += 1
         print(f"Computer Score: { computerScore }")
-        playsound('sounds/lost.mp3')
+        # playsound('sounds/lost.mp3')
+
     elif computer_choice == 1 and userchoice == "rock":
         print("Computer won!")
         computerScore += 1
         print(f"Computer Score: { computerScore }")
-        playsound('sounds/lost.mp3')
+        # playsound('sounds/lost.mp3')
+
     elif compare_choices == 2 and userchoice == "paper":
         print("Computer won!")
         computerScore += 1
         print(f"Computer Score: { computerScore }")
-        playsound('sounds/lost.mp3')
+        # playsound('sounds/lost.mp3')
+
     else:
         print("Error occured!")
 
-compare_choices()
+# compare_choices()
+
+def game_system():
+    currentGameRound = 0
+    while currentGameRound < gameRounds:
+        currentGameRound += 1
+        take_user_input()
+        check_user_input()
+        check_computer_choice()
+        compare_choices()
+
+        if currentGameRound > gameRounds:
+            break
+
+game_system()
