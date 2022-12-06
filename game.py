c28e4dccf0c408af389ced970bacb6f76459b693
PrannavP <ramsarilekhi@gmail.com>
2022-12-06 09:01:11 +0545

    added score

--- game.py
+++ game.py
@@ -1,6 +1,9 @@
 import random
 from playsound import playsound
 
+# userScore = 0
+# computerScore = 0
+
 # take user's choice
 userchoice = input("Enter your choice: ")
 lowercased_userchoice = userchoice.lower()
@@ -36,6 +39,8 @@ check_computer_choice()
 # Compare choices of computer and player
 
 def compare_choices():
+    userScore = 0
+    computerScore = 0
     # Draw case
     if computer_choice == 0 and userchoice == "rock":
         print("DRAW!")
@@ -47,23 +52,35 @@ def compare_choices():
     # User win case
     elif computer_choice == 0 and userchoice == "paper":
         print("User won!")
+        userScore += 1
+        print(f"User Score: { userScore }")
         playsound('sounds/victory.mp3')
     elif computer_choice == 1 and userchoice == "scissor":
         print("User won!")
+        userScore += 1
+        print(f"User Score: { userScore }")
         playsound('sounds/victory.mp3')
     elif computer_choice == 2 and userchoice == "rock":
         print("User won!")
+        userScore += 1
+        print(f"User Score: { userScore }")
         playsound('sounds/victory.mp3')
     
     # Computer win case
     elif computer_choice == 0 and userchoice == "scissor":
         print("Computer won!")
+        computerScore += 1
+        print(f"Computer Score: { computerScore }")
         playsound('sounds/lost.mp3')
     elif computer_choice == 1 and userchoice == "rock":
         print("Computer won!")
+        computerScore += 1
+        print(f"Computer Score: { computerScore }")
         playsound('sounds/lost.mp3')
     elif compare_choices == 2 and userchoice == "paper":
         print("Computer won!")
+        computerScore += 1
+        print(f"Computer Score: { computerScore }")
         playsound('sounds/lost.mp3')
     else:
         print("Error occured!")
